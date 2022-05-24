df62e0521f2fd02d4b62b37815563590e026f552
Ocholla-T <tonnyocholla8@gmail.com>
2022-05-24 16:26:21 +0300

    INIT: added path aliases to various part of the application

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@views': path.resolve(__dirname, './src/components/views'),
+      '@styles': path.resolve(__dirname, './src/assets/styles'),
+    },
+  },
 })
