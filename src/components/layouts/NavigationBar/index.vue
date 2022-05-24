4e20e54f8ce6e9d34e5a0d3e90716d1f8e18c072
Ocholla-T <tonnyocholla8@gmail.com>
2022-05-24 16:37:24 +0300

    INIT: added templating for the navigation bar

---
+++ src/components/layouts/NavigationBar/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="nav container flex flex-jc-sb flex-ai-c">
+    <div class="nav__logo">
+      <h1>Where in the world?</h1>
+    </div>
+    <div class="nav__dark-toggle flex flex-ai-c">
+      <img
+        class="nav__dark-toggle__icon"
+        src="../../../assets/images/moon-outline.svg"
+        alt="Turn on dark mode"
+      />
+      <p class="nav__dark-toggle__desc">Dark Mode</p>
+    </div>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+@use './styles';
+</style>
