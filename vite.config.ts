80614b00fbd807b313f8c897ff3d89981bceeeb2
Ocholla-T <tonnyocholla8@gmail.com>
2022-05-25 12:42:03 +0300

    ADD: added @store path alias

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,12 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
+      '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
       '@views': path.resolve(__dirname, './src/components/views'),
       '@styles': path.resolve(__dirname, './src/assets/styles'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@images': path.resolve(__dirname, './src/assets/images'),
     },
   },
 })
