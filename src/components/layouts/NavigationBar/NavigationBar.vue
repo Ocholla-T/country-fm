bf42ab783c76dfa28b679aebe15253c9bdeac8de
Ocholla-T <tonnyocholla8@gmail.com>
2022-05-25 12:40:58 +0300

    REFACTOR: renamed component STYLING: added dark mode functionality using useTheme state from store

---
+++ src/components/layouts/NavigationBar/NavigationBar.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+/*Dependencies */
+import { ref } from 'vue'
+import { storeToRefs } from 'pinia'
+
+// @ts-ignore
+import { useTheme } from '@store/useTheme'
+
+/*useTheme() store state and actions */
+const { isDark } = storeToRefs(useTheme())
+const { changeTheme } = useTheme()
+</script>
+
+<template>
+  <nav class="nav container flex flex-jc-sb flex-ai-c" :class="{ dark: isDark }">
+    <div class="nav__logo">
+      <h1>Where in the world?</h1>
+    </div>
+
+    <div class="nav__dark-toggle flex flex-ai-c" @click="changeTheme()">
+      <img
+        v-if="!isDark"
+        class="nav__dark-toggle__icon"
+        src="../../../assets/images/moon-outline.svg"
+        alt="Turn on dark mode"
+      />
+      <img
+        v-else
+        class="nav__dark-toggle__icon nav__dark-toggle__icon--light"
+        src="../../../assets/images/sunny-outline.svg"
+        alt="Turn on light mode"
+      />
+
+      <p class="nav__dark-toggle__desc">
+        <span v-if="!isDark">Dark</span><span v-else>Light</span> Mode
+      </p>
+    </div>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+@use './styles';
+</style>
