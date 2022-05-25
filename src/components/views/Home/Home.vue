3ece18f5506d1cddf1ee78c435a85e4fa37a6ce5
Ocholla-T <tonnyocholla8@gmail.com>
2022-05-25 09:29:10 +0300

    REFACTOR: renamed home file

---
+++ src/components/views/Home/Home.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import NavigationBar from '@components/layouts/NavigationBar/NavigationBar.vue'
+</script>
+
+<template>
+  <NavigationBar />
+</template>
--- src/components/views/Home/index.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import NavigationBar from '@components/layouts/NavigationBar/index.vue'
-</script>
-
-<template>
-  <NavigationBar />
-</template>
