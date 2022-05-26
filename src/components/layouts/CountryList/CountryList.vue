1075af7f855f45c752f881af0cd1e0ad09eecd80
Ocholla-T <tonnyocholla8@gmail.com>
2022-05-26 15:11:42 +0300

    ADD: templating for countrylist

---
+++ src/components/layouts/CountryList/CountryList.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+/*Dependencies */
+// @ts-ignore
+import { useCountries } from '@store/useCountries'
+// @ts-ignore
+import { useTheme } from '@store/useTheme'
+import { storeToRefs } from 'pinia'
+import { onBeforeMount } from 'vue'
+/*Components */
+import CountryItem from './CountryItem/CountryItem.vue'
+
+const { countries } = storeToRefs(useCountries())
+const { isDark } = storeToRefs(useTheme())
+const { fetchCountries } = useCountries()
+
+onBeforeMount(() => {
+  fetchCountries()
+})
+</script>
+
+<template>
+  <main :class="{ dark: isDark }" class="countries container grid">
+    <CountryItem :countries="countries" />
+  </main>
+</template>
+
+<style scoped lang="scss">
+@use './styles';
+</style>
