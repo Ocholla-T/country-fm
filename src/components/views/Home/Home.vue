a0249024da9ef25bce61d17b1a0427d3a4f39b05
Ocholla-T <tonnyocholla8@gmail.com>
2022-05-26 15:09:53 +0300

    ADD: added countrylist component to page layout

--- src/components/views/Home/Home.vue
+++ src/components/views/Home/Home.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import NavigationBar from '@components/layouts/NavigationBar/NavigationBar.vue'
+import CountryList from '@components/layouts/CountryList/CountryList.vue'
 </script>
 
 <template>
   <NavigationBar />
+  <CountryList />
 </template>
