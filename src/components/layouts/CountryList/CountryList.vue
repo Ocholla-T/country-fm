ce95cc4b4c711b40df33767406670e33f5868a9a
Ocholla-T <tonnyocholla8@gmail.com>
2022-05-30 14:50:19 +0300

    ADD: filterdropdown component and filtering implementation fro region

--- src/components/layouts/CountryList/CountryList.vue
+++ src/components/layouts/CountryList/CountryList.vue
@@ -9,24 +9,31 @@ import { onBeforeMount, ref } from 'vue'
 /*Components */
 import CountryItem from './CountryItem/CountryItem.vue'
 import SearchInput from '@components/ui/SearchInput/SearchInput.vue'
+import FilterDropdown from '@components/ui/FilterDropdown/FilterDropdown.vue'
 
 const { filteredCountries } = storeToRefs(useCountries())
 const { isDark } = storeToRefs(useTheme())
 const { fetchCountries } = useCountries()
-const text = ref('')
+const country = ref<string>('')
+const region = ref<string>('')
 
 onBeforeMount(() => {
   fetchCountries()
 })
+
+function fetchRegion(element: HTMLParagraphElement) {
+  region.value = element.textContent as string
+}
 </script>
 
 <template>
   <main :class="{ dark: isDark }">
-    <div class="container flex">
-      <SearchInput v-model="text" />
+    <div class="container flex flex-jc-sb form-inputs">
+      <SearchInput v-model="country" />
+      <FilterDropdown @fetchRegion="fetchRegion" />
     </div>
     <div class="countries container grid">
-      <CountryItem :countries="filteredCountries(text)" />
+      <CountryItem :countries="filteredCountries(country || region)" />
     </div>
   </main>
 </template>
