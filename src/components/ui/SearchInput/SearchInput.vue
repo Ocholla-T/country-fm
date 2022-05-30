944af47e3c10cf2544b653816faeafb32a440dae
Ocholla-T <tonnyocholla8@gmail.com>
2022-05-30 09:15:46 +0300

    ADD: styling and functionality to search input

---
+++ src/components/ui/SearchInput/SearchInput.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+// @ts-ignore
+import { useTheme } from '@store/useTheme'
+import { storeToRefs } from 'pinia'
+
+defineProps({ modelValue: String })
+
+const { isDark } = storeToRefs(useTheme())
+</script>
+
+<template>
+  <div :class="{ dark: isDark }" class="search-input flex">
+    <img
+      class="search-input__icon"
+      src="../../../assets/images/search-outline.svg"
+      alt="search icon"
+    />
+    <input
+      type="text"
+      placeholder="Search for a country..."
+      :value="modelValue"
+      @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use './styles';
+</style>
