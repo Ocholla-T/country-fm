5c6deb7f78dc1da021e3b01893589d30bc5de9fb
Ocholla-T <tonnyocholla8@gmail.com>
2022-06-05 19:55:18 +0300

    ADD: template and styling for border country button looking thingy

---
+++ src/components/views/Detail/BorderCountryCard/BorderCountryCard.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+type Country = {
+  capital: string[]
+  currencies: {
+    [key: string]: {
+      name: string
+      symbol: string
+    }
+  }
+
+  flagSource: string
+  languages: object
+  name: {
+    common: string
+    official: string
+  }
+  nativeName: {
+    [key: string]: {
+      official: string
+      common: string
+    }
+  }
+  population: string
+  region: string
+  subregion: string
+  tld: string[]
+  cca3: string
+  borders: string[]
+}
+
+defineProps<{
+  borderCountry: Country[] | undefined
+}>()
+</script>
+
+<template>
+  <div class="border-card" v-for="(country, index) in borderCountry" :key="index">
+    <p>{{ country.name.common }}</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use './styles';
+</style>
