58bf269c1858e9adde0141766118954eec035ca3
Ocholla-T <tonnyocholla8@gmail.com>
2022-06-05 19:55:51 +0300

    ADD: routing and persisted state plugin for pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import router from './router/routes'
+import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
+import router from './router'
 import App from './App.vue'
 
 createApp(App).use(createPinia()).use(router).mount('#app')
+createPinia().use(piniaPluginPersistedState)
